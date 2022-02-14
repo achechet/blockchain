c3ce40147e53a95a09e2e14f9555eb66d3791442
achechet <achechet@gmail.com>
2022-02-14 13:36:05 +0300

    FastApi UI

--- main.py
+++ main.py
@@ -21,3 +21,18 @@ def get_blockchain():
     
     chain = blockchain.chain
     return chain
+
+# endpoint to see is blockchain is valid
+@app.get("/validate/")
+def is_blockchain_valid():
+    
+    return blockchain.is_chain_valid()
+
+    
+# return previous block
+@app.get("/previous_block/")
+def previous_block():
+    if not blockchain.is_chain_valid():
+        return _fastapi.HTTPException(status_code=400, detail="THe blockchain is invalid")    
+    return blockchain.get_previous_block()
+    
